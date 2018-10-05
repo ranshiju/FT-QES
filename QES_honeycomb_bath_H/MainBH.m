43a807e84b33d18aa444cb3d5e00b5df58c1004a
ranshiju <39589534+ranshiju@users.noreply.github.com>
2018-10-05 18:55:41 +0800

    Add files via upload

--- QES_honeycomb_bath_H/MainBH.m
+++ QES_honeycomb_bath_H/MainBH.m
@@ -5,7 +5,6 @@ function MainBH(varargin)
 
 fprintf('Start running the code: QES with entanglement bath with parameters: \n');
 addpath(genpath('.'));
-addpath(genpath(['..', filesep, 'ClosedFunctions']))
 
 %% Preperation
 if(numel(varargin)==0)
@@ -16,6 +15,7 @@ end
 para.EXP=['J_(',num2str(para.Jh),',',num2str(para.Jk),')_h_(',num2str(para.hx),',',num2str(para.hz),')_dc',num2str(para.dc),'_tau',...
     num2str(para.tauDMRG),'_IsSymme',num2str(para.IsSymme)];
 para %#ok<NOPRT>
+para.tau1 = para.tau0 * para.dtau^(para.taut-1);
 
 para.PATHtree=['.',filesep,'NCDmpsHoneycomb_Av1234_withR',filesep];
 para.PATHDMRG=['.',filesep,'DMRGHoneycomb',filesep];
@@ -29,10 +29,10 @@ load([para.PATHtree,para.EXP,'.mat'],'Vx','Vy','ULx','URx','ULy','URy')
 
 %% Calculate the bath-physical interactions
 fprintf('Obtain the interactions between the physical and bath sites.  \n');
-[Hb{1,1},Hb{1,2}, ~, Hpb{1}]=Boundary_Interactions_site(Vx,URx,para.ISsvd); 
-[Hb{2,1},Hb{2,2}, ~, Hpb{2}]=Boundary_Interactions_site(Vy,URy,para.ISsvd);
-[Hb{3,1},Hb{3,2}, ~, Hpb{3}]=Boundary_Interactions_site(Vx,ULx,para.ISsvd);
-[Hb{4,1},Hb{4,2}, ~, Hpb{4}]=Boundary_Interactions_site(Vy,ULy,para.ISsvd);
+[~,~, ~, Hpb{1}]=Boundary_Interactions_site(Vx,URx,para.ISsvd, para.tau1); 
+[~,~, ~, Hpb{2}]=Boundary_Interactions_site(Vy,URy,para.ISsvd, para.tau1);
+[~,~, ~, Hpb{3}]=Boundary_Interactions_site(Vx,ULx,para.ISsvd, para.tau1);
+[~,~, ~, Hpb{4}]=Boundary_Interactions_site(Vy,ULy,para.ISsvd, para.tau1);
 
 save(['Hbath_', para.EXP], 'Hpb')
 end
